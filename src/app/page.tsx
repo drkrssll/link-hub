4e4af390e0be6a2d1923377b71bede0b0bff455a
drkrssll <russellderek01@gmail.com>
2024-09-23 22:06:09 -0400

    Added a logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
+import Image from "next/image"
+
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 lg:p-20 font-[family-name:var(--font-geist-mono)]">
+      <main className="flex flex-col gap-8 row-start-2 items-center lg:items-start">
+        <Image className="dark" src="/logo.jpg" alt="logo" width={190} height={190} priority />
+        <div className="text-lg flex gap-4 items-center flex-col lg:flex-row">
           <ol>
-            <li><a href="https://github.com/drkrssll" className="flex">{"My Github ->"}</a></li>
-            <li><a href="https://instagram.com/drkrssll" className="flex">{"My Instagram ->"}</a></li>
-            <li><a href="https://x.com/drkrssll" className="flex">{"My X / Twitter ->"}</a></li>
-            <li><a href="https://linkedin.com/" className="flex">{"My LinkedIn ->"}</a></li>
+            <li><a href="https://github.com/drkrssll" className="p-2">{"My Github ->"}</a></li>
+            <li><a href="https://instagram.com/drkrssll" className="p-2">{"My Instagram ->"}</a></li>
+            <li><a href="https://x.com/drkrssll" className="p-2">{"My X / Twitter ->"}</a></li>
+            <li><a href="https://linkedin.com/" className="p-2">{"My LinkedIn ->"}</a></li>
           </ol>
         </div>
       </main>
