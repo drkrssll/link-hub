3318b2ca8dc1b88dcb1769bc953bb1cc02a9c20c
drkrssll <russellderek01@gmail.com>
2024-09-23 22:54:45 -0400

    Put everything into a Card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,9 @@ import Image from "next/image"
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 lg:p-20 font-[family-name:var(--font-geist-mono)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center lg:items-start">
-        <Image className="dark" src="/logo.jpg" alt="logo" width={190} height={190} priority />
-        <div className="text-lg flex gap-4 items-center flex-col lg:flex-row">
+      <main className="border rounded-lg p-4 flex flex-col gap-8 row-start-2 items-center lg:items-start">
+        <Image className="dark" src="/logo.jpg" alt="logo" width={200} height={200} priority />
+        <div className="text-lg flex gap-2 items-center flex-col lg:flex-row">
           <ol>
             <li><a href="https://github.com/drkrssll" className="p-2">{"My Github ->"}</a></li>
             <li><a href="https://instagram.com/drkrssll" className="p-2">{"My Instagram ->"}</a></li>
