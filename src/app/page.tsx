7d799568c5229ecafc33c1c9e5b43b40312cd7a8
drkrssll <russellderek01@gmail.com>
2024-09-30 20:08:46 -0400

    Fixed mobile layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <>
       <DisplayParticles id="particles" />
-      <div className="min-h-screen font-[family-name:var(--font-geist-mono)] grid grid-rows-[20px_1fr_20px] items-center justify-items-center p-8 gap-16 relative z-10">
+      <div className="min-h-screen font-[family-name:var(--font-geist-mono)] grid grid-rows-[20px_1fr_20px] items-center justify-items-center p-8 relative z-10">
         <main
           className="
             border-2
